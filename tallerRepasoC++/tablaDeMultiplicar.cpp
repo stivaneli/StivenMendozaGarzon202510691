134876dad4c9788b6edde61a01d2d576571aaef8
stivaneli <miguel.mendoza03@uptc.edu.co>
2025-09-25 15:29:23 +0000

    Update

--- tallerRepasoC++/tablaDeMultiplicar.cpp
+++ tallerRepasoC++/tablaDeMultiplicar.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void mostrarTabla(int number) {
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 10; i >= 1; i--) {
         cout << number << " x " << i << " = " << number * i << endl;
     }
 }
